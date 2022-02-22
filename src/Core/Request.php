f4910477e0d194e57db3fdb6a3a58a5b31ad9e90
ellisgl <xelliss@gmail.com>
2022-02-21 20:41:46 -0600

    Add a JSON content decoder for requests.

---
+++ src/Core/Request.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Core;
+
+use JsonException;
+
+class Request extends \Symfony\Component\HttpFoundation\Request
+{
+    /**
+     * Get content from request (POST, PUT, PATCH typically), and JSON decodes it into an array.
+     * @return array
+     * @throws JsonException
+     */
+    public function getJsonContentAsArray(): array
+    {
+        return json_decode($this->getContent(), true, 512, JSON_THROW_ON_ERROR);
+    }
+}
