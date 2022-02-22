c1500185553285602ac693f00684b7a8c78fc0ba
ellisgl <xelliss@gmail.com>
2022-02-22 08:14:47 -0600

    Starting tests for error stuff.

---
+++ tests/unit/Core/Http/HttpErrorServiceTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace unit\Core\Http;
+
+use App\Core\Http\Exceptions\HttpBadRequestException;
+use App\Core\Http\HttpErrorService;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class HttpErrorServiceTest extends TestCase
+{
+    private HttpErrorService $httpErrorService;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->httpErrorService = new HttpErrorService();
+    }
+
+    /**
+     * This will test most of the error response generators full, except the 500 generator.
+     *
+     * @return void
+     */
+    public function testHandleError(): void
+    {
+        $res = $this->httpErrorService->handleError(new HttpBadRequestException());
+        self::assertEquals(JsonResponse::HTTP_BAD_REQUEST, $res->getStatusCode());
+        self::assertEquals('400 - Bad request', $res->getContent());
+
+        $res = $this->httpErrorService->handleError(new HttpBadRequestException('Test'));
+        self::assertEquals(JsonResponse::HTTP_BAD_REQUEST, $res->getStatusCode());
+        self::assertEquals('Test', $res->getContent());
+    }
+}
