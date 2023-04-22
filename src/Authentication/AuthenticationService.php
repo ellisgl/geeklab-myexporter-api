f706bf6967b0fa5d49e4c2703d518dff78d83d22
EllisGl <xellisx@gmail.com>
2023-04-21 21:18:23 -0500

    PHP 8.1 constructor Docs Code style.

--- src/Authentication/AuthenticationService.php
+++ src/Authentication/AuthenticationService.php
@@ -16,17 +16,11 @@ use JsonException;
 
 class AuthenticationService
 {
-    private GLConf $config;
-
-    private PdoService $dbService;
-
     // Decoded JWT object.
     private ?object $token = null;
 
-    public function __construct(GLConf $config, PdoService $dbService)
+    public function __construct(private readonly GLConf $config, private readonly PdoService $dbService)
     {
-        $this->config = $config;
-        $this->dbService = $dbService;
     }
 
     /**
@@ -42,13 +36,13 @@ class AuthenticationService
         // Decode the request.
         $data = $request->getJsonContentAsArray();
 
-        // Attempt a connection
+        // Attempt an authenticated connection to the database.
         try {
             $this->dbService->createPDO(
-                $this->config->get('servers.' . (int) $data['server_id'] . '.host'),
+                $this->config->get('servers.' . (int)$data['server_id'] . '.host'),
                 $data['username'],
                 $data['password'],
-                $this->config->get('servers.' . (int) $data['server_id'] . '.port') ?: 3306
+                $this->config->get('servers.' . (int)$data['server_id'] . '.port') ?: 3306,
             );
         } catch (Exception) {
             throw new HttpUnauthorizedException();
@@ -56,28 +50,33 @@ class AuthenticationService
 
         // Create a JWT token and return it.
         $iat = time();
+
         return JWT::encode(
             [
                 'iss' => 'localhost',
-                'aud' => 'myExporter: ' . $this->config->get('servers.' . (int) $data['server_id'] . '.name'),
+                'aud' => 'myExporter: ' . $this->config->get('servers.' . (int)$data['server_id'] . '.name'),
                 'iat' => $iat,
                 'nbf' => $iat,
                 'exp' => $iat + 86400,
                 'hash' => sha1($request->getClientIp()),
                 'data' => [
-                    'host' => (int) $data['server_id'],
-                    'dbh'  => $this->config->get('servers.' . (int) $data['server_id'] . '.host'),
-                    'dbu'  => $data['username'],
-                    'dbp'  => $data['password'],
-                    'port' => $this->config->get('servers.' . (int) $data['server_id'] . '.port') ?: 3306,
-                ]
+                    'host' => (int)$data['server_id'],
+                    'dbh' => $this->config->get('servers.' . (int)$data['server_id'] . '.host'),
+                    'dbu' => $data['username'],
+                    'dbp' => $data['password'],
+                    'port' => $this->config->get('servers.' . (int)$data['server_id'] . '.port') ?: 3306,
+                ],
             ],
             $this->config->get('jwt.secret_key'),
-            $this->config->get('jwt.alg')
+            $this->config->get('jwt.alg'),
         );
     }
 
     /**
+     * Check if the request is authenticated.
+     *
+     * @param Request $request
+     *
      * @throws HttpUnauthorizedException
      */
     public function isAuthenticated(Request $request): void
@@ -99,6 +98,8 @@ class AuthenticationService
     }
 
     /**
+     * Return the decoded JWT object.
+     *
      * @param Request $request
      *
      * @return object | null
@@ -115,7 +116,7 @@ class AuthenticationService
                     $now = new DateTimeImmutable();
                     $token = JWT::decode(
                         $jwt,
-                        new Key($this->config->get('jwt.secret_key'), $this->config->get('jwt.alg'))
+                        new Key($this->config->get('jwt.secret_key'), $this->config->get('jwt.alg')),
                     );
                     $hash = sha1($request->getClientIp());
                     $this->token = $token->nbf > $now->getTimestamp() || $token->exp < $now->getTimestamp(
